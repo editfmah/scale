5d9ff988b69bcb6f80bc83d1105387e0bbcfce18
Adrian Herridge <ah@Adrians-MacBook-Pro.local>
2017-01-29 15:23:22 +0000

    got something building on macOS
    Signed-off-by: Adrian Herridge <ah@Adrians-MacBook-Pro.local>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "scale",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0),
         .Package(url: "https://github.com/sharksync/Swift-Kuery-SQLite.git", majorVersion: 0)
     ]
 )
