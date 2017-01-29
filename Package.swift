38c955aba361b1d45ca948ba000c0f140d583d2e
sharkorm <devs@sharkorm.com>
2017-01-29 13:33:02 +0000

    .

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "scale",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/sharksync/SQLite.swift.git", majorVersion:0)
+        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0),
+        .Package(url: "https://github.com/sharksync/Swift-Kuery-SQLite.git", majorVersion: 0)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -3,7 +3,6 @@ import Foundation
 import Kitura
 import KituraNet
 import SwiftyJSON
-import SQLite
 
 let version = 0.00
 
@@ -49,7 +48,33 @@ router.route("/").post {
 
 // test sqlite is working on linux 
 
+let db = try Connection("test.sqlite3")
 
+let users = Table("users")
+let id = Expression<Int64>("id")
+let name = Expression<String?>("name")
+let email = Expression<String>("email")
+
+try db.run(users.create { t in
+    t.column(id, primaryKey: true)
+    t.column(name)
+    t.column(email, unique: true)
+})
+// CREATE TABLE "users" (
+//     "id" INTEGER PRIMARY KEY NOT NULL,
+//     "name" TEXT,
+//     "email" TEXT NOT NULL UNIQUE
+// )
+
+let insert = users.insert(name <- "Alice", email <- "alice@mac.com")
+let rowid = try db.run(insert)
+// INSERT INTO "users" ("name", "email") VALUES ('Alice', 'alice@mac.com')
+
+for user in try db.prepare(users) {
+    print("id: \(user[id]), name: \(user[name]), email: \(user[email])")
+    // id: 1, name: Optional("Alice"), email: alice@mac.com
+}
+// SELECT * FROM "users"
 
 // Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: nodeSettings.queryPort!, with: router)
