26c4a6f18bd5073a36a4a61e5afbc38bc2d83574
Adrian Herridge <editfmah@yahoo.co.uk>
2017-11-03 22:36:17 +0000

    Revert SwiftJSON, for compatibility on Linux

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
         .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
         .Package(url: "https://github.com/sharksync/SWSQLite.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 3)
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16)
     ]
 )
